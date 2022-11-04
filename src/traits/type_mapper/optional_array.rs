d0426b3db90f251814ba19d23859620b3a43b9b9
u-kai <76635578+u-kai@users.noreply.github.com>
2022-11-04 21:17:07 +0900

    add optional case and array case and optional array case

---
+++ src/traits/type_mapper/optional_array.rs
@@ -0,0 +1,8 @@
+pub trait OptionalPrimitiveArray {
+    fn case_string(&self) -> String;
+    fn case_null(&self) -> String;
+    fn case_i64(&self) -> String;
+    fn case_u64(&self) -> String;
+    fn case_f64(&self) -> String;
+    fn case_bool(&self) -> String;
+}
