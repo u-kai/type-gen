e853218a1b8474df0745d875c348e271d3c98426
u-kai <76635578+u-kai@users.noreply.github.com>
2022-11-11 06:59:59 +0900

    add skip_character and parser trait

--- src/lang_parsers/parsers.rs
+++ src/lang_parsers/parsers.rs
@@ -1,8 +1,23 @@
+pub trait Parser<T>: Fn(&str) -> Option<(T, &str)> {}
+impl<T, F> Parser<T> for F where F: Fn(&str) -> Option<(T, &str)> {}
+fn add_lifetime<T, F>(f: F) -> F
+where
+    F: Fn(&str) -> Option<(T, &str)>,
+{
+    f
+}
+pub fn skip_character(c: char) -> impl Parser<()> {
+    add_lifetime(move |s| {
+        let mut chars = s.chars();
+        if chars.next() == Some(c) {
+            return None;
+        };
+        Some(((), chars.as_str()))
+    })
+}
+
 pub fn trim_head<'a>(data: &'a str) -> Option<((), &'a str)> {
-    let new_len = data
-        .chars()
-        .skip_while(|c| c.is_whitespace() || c.is_ascii_whitespace())
-        .count();
+    let new_len = data.chars().skip_while(|c| c.is_whitespace()).count();
     let start = data.len() - new_len;
     Some(((), &data[start..]))
 }
