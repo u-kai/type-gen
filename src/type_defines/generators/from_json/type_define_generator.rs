f60c1a05f9d547bb0b381959237b2b130c48a5eb
u-kai <76635578+u-kai@users.noreply.github.com>
2022-11-17 22:07:40 +0900

    add tests dir

--- src/type_defines/generators/from_json/type_define_generator.rs
+++ src/type_defines/generators/from_json/type_define_generator.rs
@@ -338,28 +338,8 @@ where
 #[cfg(test)]
 mod test_type_define_gen {
 
-    use std::cell::RefCell;
-
-    use crate::type_defines::generators::from_json::{
-        lang_common::{
-            field_statements::field_comment::BaseFieldComment,
-            primitive_type_statement_generator::FakeMapper,
-            type_statements::type_comment::BaseTypeComment,
-        },
-        rust::{
-            field_statements::{
-                field_attr::RustFieldAttributeStore, field_statement::RustfieldStatement,
-                field_visibility::RustFieldVisibilityProvider, reserved_words::RustReservedWords,
-            },
-            json_lang_mapper::JsonRustMapper,
-            off_side_rule::RustOffSideRule,
-            rust_visibility::RustVisibility,
-            type_statements::{
-                type_attr::{RustTypeAttribute, RustTypeAttributeStore},
-                type_statement::RustTypeStatement,
-                type_visibility::RustTypeVisibilityProvider,
-            },
-        },
+    use crate::type_defines::generators::from_json::lang_common::{
+        primitive_type_statement_generator::FakeMapper,
     };
 
     use super::*;
@@ -794,78 +774,6 @@ struct TestObj {
 
 "#;
         assert_eq!(type_gen.gen_from_json(json), tobe.to_string());
-    }
-    #[test]
-    fn test_case_rust() {
-        let json = r#"
-            {
-                "data":[
-                    {
-                        "userId":12345,
-                        "test":"test-string",
-                        "entities":{
-                            "id":0
-                        }
-                    }
-                ]
-            }
-        "#;
-        let tobe = r#"#[derive(Serialize,Desrialize)]
-pub struct TestJson {
-    data: Vec<TestJsonData>,
-}
-
-#[derive(Serialize,Desrialize)]
-struct TestJsonData {
-    entities: Option<TestJsonDataEntities>,
-    test: Option<String>,
-    #[serde(rename = "userId")]
-    user_id: Option<i64>,
-}
-
-#[derive(Serialize,Desrialize)]
-pub struct TestJsonDataEntities {
-    id: Option<i64>,
-}
-
-"#
-        .to_string();
-        let mut optional_checker = BaseOptionalChecker::default();
-        optional_checker.add_require("TestJson", "data");
-        let t_comment = BaseTypeComment::new("//");
-        let mut t_attr = RustTypeAttributeStore::new();
-        t_attr.add_attr(
-            "TestJson",
-            RustTypeAttribute::Derive(vec!["Serialize".to_string(), "Desrialize".to_string()]),
-        );
-        t_attr.add_attr(
-            "TestJsonData",
-            RustTypeAttribute::Derive(vec!["Serialize".to_string(), "Desrialize".to_string()]),
-        );
-        t_attr.add_attr(
-            "TestJsonDataEntities",
-            RustTypeAttribute::Derive(vec!["Serialize".to_string(), "Desrialize".to_string()]),
-        );
-        let mut t_visi = RustTypeVisibilityProvider::new();
-        t_visi.add_visibility("TestJson", RustVisibility::Public);
-        t_visi.add_visibility("TestJsonDataEntities", RustVisibility::Public);
-        let rw = RustReservedWords::new();
-        let f_comment = BaseFieldComment::new("//");
-        let f_attr = RustFieldAttributeStore::new();
-        let f_visi = RustFieldVisibilityProvider::new();
-        let osr = RustOffSideRule::new();
-        let mapper = JsonRustMapper::new();
-        let t_statement = RustTypeStatement::new(t_comment, t_visi, t_attr);
-        let f_statement = RustfieldStatement::new(f_comment, RefCell::new(f_attr), f_visi, rw);
-        let rust = TypeDefineGenerator::new(
-            "TestJson",
-            mapper,
-            t_statement,
-            f_statement,
-            osr,
-            optional_checker,
-        );
-        assert_eq!(rust.gen_from_json(json), tobe);
     }
 }
 
