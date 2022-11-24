af396c86c7324575c4faef5c19e808960bddcf29
u-kai <76635578+u-kai@users.noreply.github.com>
2022-11-24 21:36:12 +0900

    add set_all at comment and optional

--- lang-common/src/type_defines/additional_defines/comment_store.rs
+++ lang-common/src/type_defines/additional_defines/comment_store.rs
@@ -12,6 +12,8 @@ pub struct CommentStore<C>
 where
     C: Comment,
 {
+    all_type_comment: Option<C>,
+    all_property_comment: Option<C>,
     type_store: HashMap<TypeName, C>,
     property_store: HashMap<(TypeName, PropertyKey), C>,
 }
@@ -22,9 +24,17 @@ where
 {
     pub fn new() -> Self {
         Self {
+            all_type_comment: None,
+            all_property_comment: None,
             type_store: HashMap::new(),
             property_store: HashMap::new(),
         }
+    }
+    pub fn set_all_type_comment(&mut self, comment: C) {
+        self.all_type_comment = Some(comment)
+    }
+    pub fn set_all_property_comment(&mut self, comment: C) {
+        self.all_property_comment = Some(comment)
     }
     pub fn add_type_comment(&mut self, type_name: impl Into<TypeName>, comment: C) {
         self.type_store.insert(type_name.into(), comment);
@@ -39,6 +49,9 @@ where
             .insert((type_name.into(), property_key.into()), comment);
     }
     pub fn get_type_comment(&self, type_name: &TypeName) -> Option<String> {
+        if self.all_type_comment.is_some() {
+            return self.all_type_comment.as_ref().map(|c| c.to_type_define());
+        }
         self.type_store
             .get(type_name)
             .map(|comment| comment.to_type_define())
@@ -48,6 +61,12 @@ where
         type_name: &TypeName,
         property_key: &PropertyKey,
     ) -> Option<String> {
+        if self.all_property_comment.is_some() {
+            return self
+                .all_property_comment
+                .as_ref()
+                .map(|c| c.to_property_define());
+        }
         get_tuple_key_store(&self.property_store, type_name, property_key)
             .map(|comment| comment.to_property_define())
     }
