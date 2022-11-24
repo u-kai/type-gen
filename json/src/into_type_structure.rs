abe9098a46a73c103f94a0f99ad6223bb684e13a
u-kai <76635578+u-kai@users.noreply.github.com>
2022-11-24 10:11:47 +0900

    clear nest obj

--- json/src/into_type_structure.rs
+++ json/src/into_type_structure.rs
@@ -1,4 +1,4 @@
-use std::collections::BTreeMap;
+use std::collections::{BTreeMap, VecDeque};
 
 use lang_common::types::{
     primitive_type::primitive_type_factories::{
@@ -32,10 +32,13 @@ impl Json {
         root_name: impl Into<TypeName>,
         obj: BTreeMap<String, Self>,
     ) -> Vec<TypeStructure> {
+        // Test
         let root_name = root_name.into();
-        let mut result = Vec::new();
+        let mut result = VecDeque::new();
+        // id:usize , obj: TestObj , name: String ,
         let mut root_properties = BTreeMap::new();
         for (key, json) in obj {
+            // obj
             let property_key = PropertyKey::from(key);
             let (property_type, children) = match json {
                 Json::String(_) => (make_primitive_type(make_string()), None),
@@ -43,9 +46,12 @@ impl Json {
                 Json::Boolean(_) => (make_primitive_type(make_bool()), None),
                 Json::Null => (make_any(), None),
                 Json::Object(obj) => {
+                    // TestObj
                     let type_name = property_key.to_type_name(&root_name);
-                    let childrens = Self::obj_to_property_types(&type_name, obj);
-                    let property_type = PropertyType::new_custom_type(type_name);
+                    // TestObj childrens
+                    let childrens = Self::obj_to_childrens(&type_name, obj);
+                    // Custom TestObj
+                    let property_type = Self::type_name_to_property_type(type_name);
                     (property_type, Some(childrens))
                 }
                 Json::Array(_) => todo!(),
@@ -55,14 +61,50 @@ impl Json {
         }
         let root =
             TypeStructure::Composite(CompositeTypeStructure::new(root_name, root_properties));
-        result.insert(0, root);
+        result.push_front(root);
+        result.into()
+    }
+    fn obj_to_childrens(
+        // TestObj
+        type_name: &TypeName,
+        obj: BTreeMap<String, Self>,
+    ) -> VecDeque<TypeStructure> {
+        let mut result = VecDeque::new();
+        let mut properties = BTreeMap::new();
+        for (key, json) in obj {
+            // data
+            let property_key = PropertyKey::from(key);
+            match json {
+                Json::String(_) => {
+                    properties.insert(property_key, make_primitive_type(make_string()));
+                }
+                Json::Number(num) => {
+                    properties.insert(property_key, Self::json_num_to_property_type(num));
+                }
+                Json::Boolean(_) => {
+                    properties.insert(property_key, make_primitive_type(make_bool()));
+                }
+                Json::Null => {
+                    properties.insert(property_key, make_any());
+                }
+                Json::Object(obj) => {
+                    // TestObjData
+                    let type_name = property_key.to_type_name(&type_name);
+                    let mut childrens = Self::obj_to_childrens(&type_name, obj);
+                    let property_type = PropertyType::new_custom_type(type_name);
+                    result.append(&mut childrens);
+                    properties.insert(property_key, property_type);
+                }
+                Json::Array(_) => todo!(),
+            }
+        }
+        let type_structure =
+            TypeStructure::Composite(CompositeTypeStructure::new(type_name, properties));
+        result.push_front(type_structure);
         result
     }
-    fn obj_to_property_types(
-        parent_name: &TypeName,
-        obj: BTreeMap<String, Self>,
-    ) -> Vec<TypeStructure> {
-        vec![]
+    fn type_name_to_property_type(type_name: TypeName) -> PropertyType {
+        PropertyType::new_custom_type(type_name)
     }
     fn case_arr(arr: Vec<Self>) -> Vec<TypeStructure> {
         vec![]
@@ -103,10 +145,35 @@ impl Json {
 mod test_into_type_structures {
     use lang_common::types::{
         primitive_type::primitive_type_factories::make_string,
-        property_type::property_type_factories::make_primitive_type,
+        property_type::property_type_factories::{make_custom_type, make_primitive_type},
     };
 
     use super::*;
+    #[test]
+    fn test_nest_obj_case() {
+        let name = "Test";
+        let json = Json::from(r#"{"name":"kai","obj":{"id":0,"name":"kai"}}"#);
+        let tobe = vec![
+            TypeStructure::make_composite(
+                name,
+                vec![
+                    ("name", make_primitive_type(make_string())),
+                    ("obj", make_custom_type("TestObj")),
+                ],
+            ),
+            TypeStructure::make_composite(
+                "TestObj",
+                vec![
+                    ("id", make_primitive_type(make_usize())),
+                    ("name", make_primitive_type(make_string())),
+                ],
+            ),
+        ];
+        let expect = json.into_type_structures(name);
+        println!("expect : {:#?}", expect);
+        println!("tobe : {:#?}", tobe);
+        assert_eq!(expect, tobe);
+    }
     #[test]
     fn test_simple_case() {
         let name = "Test";
