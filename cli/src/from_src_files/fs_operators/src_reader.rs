de65b01deaa397ecc1e88b0379b47462342c5beb
u-kai <76635578+u-kai@users.noreply.github.com>
2022-11-27 11:08:20 +0900

    clear read_all_srcs

--- cli/src/from_src_files/fs_operators/src_reader.rs
+++ cli/src/from_src_files/fs_operators/src_reader.rs
@@ -1,7 +1,22 @@
-use std::path::Path;
+use std::{fs::read_to_string, io, path::Path};
 
-use super::util_fns::is_dir;
-
+pub struct TypeDefineSrcReader<P: AsRef<Path>> {
+    all_src_paths: Vec<P>,
+}
+impl<P: AsRef<Path>> TypeDefineSrcReader<P> {
+    pub fn new(all_src_paths: Vec<P>) -> Self {
+        Self { all_src_paths }
+    }
+    pub fn read_all_srcs(self) -> impl Iterator<Item = SrcFile<P>> {
+        self.all_src_paths
+            .into_iter()
+            .filter_map(|src| Self::src_file(src).ok())
+    }
+    fn src_file(filepath: P) -> io::Result<SrcFile<P>> {
+        let contents = read_to_string(filepath.as_ref())?;
+        Ok(SrcFile::new(filepath, contents))
+    }
+}
 pub struct SrcFile<P>
 where
     P: AsRef<Path>,
@@ -25,33 +40,34 @@ where
         let extension = format!(".{}", self.path.as_ref().extension()?.to_str()?);
         Some(with_extension.to_string_lossy().replace(&extension, ""))
     }
-    pub fn contents(&self) -> &str {
+    pub fn content(&self) -> &str {
         &self.content
     }
-}
-pub struct TypeDefineSrcReader<P: AsRef<Path>> {
-    all_src_files: Vec<P>,
-}
-impl<P: AsRef<Path>> TypeDefineSrcReader<P> {
-    pub fn new(all_src_files: Vec<P>) -> Self {
-        Self { all_src_files }
-    }
-    //pub fn all_src_filename_and_contents(&self) -> Vec<(String, String)> {
-    //self.all_src_files
-    //.iter()
-    //.map(|src| self.filename_and_contents(src))
-    //.collect()
-    //}
-    //fn filename_and_contents<P: AsRef<Path>>(&self, filepath: P) -> (String, String) {
-    //let filepath = filepath.as_ref();
-    //let contents = read_to_string(filepath).unwrap();
-    //(extract_filename(filepath), contents)
-    //}
 }
 
 #[cfg(test)]
 mod test_src_file {
-    use super::SrcFile;
+    use super::{SrcFile, TypeDefineSrcReader};
+    #[test]
+    // this test context is exist test dir
+    fn test_read_all_srcs() {
+        let all_src_paths = vec![
+            "src/from_src_files/test/parent.txt",
+            "src/from_src_files/test/child/child.txt",
+            "src/from_src_files/test/child/grand_child/grand_child.txt",
+        ];
+        let src_reader = TypeDefineSrcReader::new(all_src_paths);
+        let mut all_srcs = src_reader.read_all_srcs();
+        let src = all_srcs.next().unwrap();
+        assert_eq!(src.extract_filename().unwrap(), "parent".to_string());
+        assert_eq!(src.content(), "this is parent");
+        let src = all_srcs.next().unwrap();
+        assert_eq!(src.extract_filename().unwrap(), "child".to_string());
+        assert_eq!(src.content(), "this is child");
+        let src = all_srcs.next().unwrap();
+        assert_eq!(src.extract_filename().unwrap(), "grand_child".to_string());
+        assert_eq!(src.content(), "this is grand_child");
+    }
 
     #[test]
     fn test_extract_filename() {
