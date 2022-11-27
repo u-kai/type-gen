5ae194d1b4e0e0c218f6876db73796f60a931f13
u-kai <76635578+u-kai@users.noreply.github.com>
2022-11-27 12:36:00 +0900

    add src_paths and fix dist_dir_maker use src_paths

--- cli/src/from_src_files/fs_operators/dist_dir_maker.rs
+++ cli/src/from_src_files/fs_operators/dist_dir_maker.rs
@@ -1,80 +1,82 @@
-use std::{collections::BTreeSet, path::Path};
+use std::collections::BTreeSet;
 
-use super::util_fns::{extract_dir, mkdir};
+use super::{
+    src_paths::SrcPaths,
+    util_fns::{extract_dir, mkdir_rec},
+};
 
 pub struct TypeDefineDistDirectoriesMaker<'a> {
-    src: &'a str,
+    src: &'a SrcPaths<'a>,
     dist: &'a str,
 }
 
 impl<'a> TypeDefineDistDirectoriesMaker<'a> {
-    pub fn new(src: &'a str, dist: &'a str) -> Self {
+    pub fn new(src: &'a SrcPaths<'a>, dist: &'a str) -> Self {
         Self { src, dist }
     }
-    fn gen_all_dist_dir_path<P: AsRef<Path>>(
-        &self,
-        all_src_files: &'a Vec<P>,
-    ) -> impl Iterator<Item = String> + '_ {
-        all_src_files
+    pub fn make_dist_dirs(&self) {
+        self.gen_all_dist_dir_path()
+            .for_each(|dir| mkdir_rec(dir).unwrap());
+    }
+    fn gen_all_dist_dir_path(&self) -> impl Iterator<Item = String> + '_ {
+        self.src
+            .all_src()
             .iter()
             .filter_map(|p| extract_dir(p))
-            .map(|dir| dir.replace(self.src, self.dist))
+            .map(|dir| dir.replace(self.src.src(), self.dist))
             .collect::<BTreeSet<_>>()
             .into_iter()
     }
-    //pub fn create_dist_directories(&self, all_src_files: &Vec<PathBuf>) {
-    //self.generate_all_dist_directory_path(all_src_files)
-    //.for_each(|dir| mkdir(dir));
-    //}
-    //fn generate_all_dist_directory_path(
-    //&self,
-    //all_src_files: &Vec<PathBuf>,
-    //) -> impl Iterator<Item = String> + '_ {
-    //all_src_files
-    //.into_iter()
-    //.map(|src| extract_directory_part_from_path(src).replace(self.src, self.dist))
-    //.flat_map(|dist| split_dirs(dist))
-    //.into_iter()
-    //.collect::<BTreeSet<_>>()
-    //.into_iter()
-    //}
 }
 
 #[cfg(test)]
 mod test_dist_dir_maker {
+    use crate::from_src_files::fs_operators::src_paths::SrcPaths;
+
     use super::TypeDefineDistDirectoriesMaker;
 
     #[test]
     fn test_gen_all_dist_dir_path() {
-        let maker = TypeDefineDistDirectoriesMaker::new("src", "dist");
-        let all_src_paths = vec![
-            "./src/test.txt",
-            "./src/dir1/test.txt",
-            "./src/dir2/test.txt",
-        ];
-        let mut dist_dirs = maker.gen_all_dist_dir_path(&all_src_paths);
+        let src_path = SrcPaths::for_test(
+            "src",
+            vec![
+                "./src/test.txt",
+                "./src/dir1/test.txt",
+                "./src/dir2/test.txt",
+            ],
+        );
+        let maker = TypeDefineDistDirectoriesMaker::new(&src_path, "dist");
+        let mut dist_dirs = maker.gen_all_dist_dir_path();
         assert_eq!(dist_dirs.next().unwrap(), "./dist/");
         assert_eq!(dist_dirs.next().unwrap(), "./dist/dir1/");
         assert_eq!(dist_dirs.next().unwrap(), "./dist/dir2/");
         assert_eq!(dist_dirs.next(), None);
-        let all_src_paths = vec![
-            "./src/test.txt",
-            "./src/test2.txt",
-            "./src/dir1/test.txt",
-            "./src/dir2/test.txt",
-        ];
-        let mut dist_dirs = maker.gen_all_dist_dir_path(&all_src_paths);
+        let src_path = SrcPaths::for_test(
+            "src",
+            vec![
+                "./src/test.txt",
+                "./src/test2.txt",
+                "./src/dir1/test.txt",
+                "./src/dir2/test.txt",
+            ],
+        );
+        let maker = TypeDefineDistDirectoriesMaker::new(&src_path, "dist");
+        let mut dist_dirs = maker.gen_all_dist_dir_path();
         assert_eq!(dist_dirs.next().unwrap(), "./dist/");
         assert_eq!(dist_dirs.next().unwrap(), "./dist/dir1/");
         assert_eq!(dist_dirs.next().unwrap(), "./dist/dir2/");
         assert_eq!(dist_dirs.next(), None);
-        let all_src_paths = vec![
-            "./src/test.txt",
-            "./src/dir1/test.txt",
-            "./src/dir2/test.txt",
-            "./src/dir2/dir3/dir4/dir5/test.txt",
-        ];
-        let mut dist_dirs = maker.gen_all_dist_dir_path(&all_src_paths);
+        let src_path = SrcPaths::for_test(
+            "src",
+            vec![
+                "./src/test.txt",
+                "./src/dir1/test.txt",
+                "./src/dir2/test.txt",
+                "./src/dir2/dir3/dir4/dir5/test.txt",
+            ],
+        );
+        let maker = TypeDefineDistDirectoriesMaker::new(&src_path, "dist");
+        let mut dist_dirs = maker.gen_all_dist_dir_path();
         assert_eq!(dist_dirs.next().unwrap(), "./dist/");
         assert_eq!(dist_dirs.next().unwrap(), "./dist/dir1/");
         assert_eq!(dist_dirs.next().unwrap(), "./dist/dir2/");
