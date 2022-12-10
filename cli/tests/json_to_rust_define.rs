94bb196f69511ae3a78bbffe0dfceb40b2e31c54
u-kai <76635578+u-kai@users.noreply.github.com>
2022-12-10 16:20:42 +0900

    add typedefineconfigfile

--- cli/tests/json_to_rust_define.rs
+++ cli/tests/json_to_rust_define.rs
@@ -11,21 +11,10 @@ use cli::from_src_files::{
 
 #[test]
 fn test_json_to_rust_define() {
-    //
-    let config = r#"
-    {
-        "src": {
-            "root": "examples/jsons",
-            "extension": "json"
-        },
-        "dist": {
-            "root": "examples/dist",
-            "extension": "rs"
-        }
-    }
-    "#;
-    let config_file = "tests/type-config.json";
-    write_new_file(config_file, config);
+    //set up config
+    let config = TypeDefineConfigFile::default();
+    config.write_config_file();
+    config.clean_up();
     let source_files = [
         "tests/json/parent.json",
         "tests/json/child/child.json",
@@ -33,11 +22,46 @@ fn test_json_to_rust_define() {
     ];
     mkdir_from_filepath(&source_files);
     delete_dirs("tests/json");
-    delete_file(config_file)
     //let src_file1=
     // set up source of json
 
     //json_to_rust_define("tests/type-define-config.json")
+}
+
+struct TypeDefineConfigFile<'a> {
+    config_file: &'a str,
+    content: &'a str,
+}
+impl<'a> TypeDefineConfigFile<'a> {
+    fn new(config_file: &'a str, content: &'a str) -> Self {
+        Self {
+            config_file,
+            content,
+        }
+    }
+    fn write_config_file(&self) {
+        write_new_file(self.config_file, self.content);
+    }
+    fn clean_up(self) {
+        delete_file(self.config_file)
+    }
+}
+impl<'a> Default for TypeDefineConfigFile<'a> {
+    fn default() -> Self {
+        let content = r#"
+            {
+                "src": {
+                    "root": "examples/jsons",
+                    "extension": "json"
+                },
+                "dist": {
+                    "root": "examples/dist",
+                    "extension": "rs"
+                }
+            }
+        "#;
+        TypeDefineConfigFile::new("tests/type-define-config.json", content)
+    }
 }
 
 fn mkdir_from_filepath<P: AsRef<Path>>(paths: &[P]) {
