d85fa3003d1b53a8d6539891bf8abed2b1194f04
u-kai <76635578+u-kai@users.noreply.github.com>
2023-01-10 06:40:41 +0900

    add property_type

--- type_define/structure/src/parts/property_type.rs
+++ type_define/structure/src/parts/property_type.rs
@@ -1,27 +1,85 @@
-use npc::convertor::NamingPrincipalConvertor;
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
-pub struct TypeName(String);
-impl TypeName {
-    pub fn new(str: String) -> Self {
-        Self(str)
+use super::type_name::TypeName;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum PropertyType {
+    Array(Box<PropertyType>),
+    Optional(Box<PropertyType>),
+    Primitive(PrimitiveType),
+    CustomType(TypeName),
+    Any,
+}
+impl PropertyType {
+    pub fn new_custom_type(type_name: impl Into<TypeName>) -> Self {
+        Self::CustomType(type_name.into())
     }
-    pub fn as_str(&self) -> &str {
-        &self.0
+    pub fn to_optional(self) -> Self {
+        Self::Optional(Box::new(self))
+    }
+    pub fn to_array(self) -> Self {
+        Self::Array(Box::new(self))
     }
 }
-impl<I> From<I> for TypeName
-where
-    I: Into<String>,
-{
-    fn from(str: I) -> Self {
-        let str: String = str.into();
-        let str = NamingPrincipalConvertor::new(&str).to_pascal();
-        TypeName::new(str)
-    }
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum PrimitiveType {
+    String,
+    Boolean,
+    Number(Number),
+}
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum Number {
+    Usize,
+    Isize,
+    Float,
 }
 
-impl From<&TypeName> for TypeName {
-    fn from(ref_: &TypeName) -> Self {
-        ref_.as_str().into()
+pub mod property_type_factories {
+    use super::*;
+    pub fn make_array_type(property_type: PropertyType) -> PropertyType {
+        PropertyType::Array(Box::new(property_type))
+    }
+    pub fn make_optional_type(property_type: PropertyType) -> PropertyType {
+        PropertyType::Optional(Box::new(property_type))
+    }
+    pub fn make_custom_type(type_name: impl Into<TypeName>) -> PropertyType {
+        PropertyType::CustomType(type_name.into())
+    }
+    pub fn make_primitive_type(primitive_type: PrimitiveType) -> PropertyType {
+        PropertyType::Primitive(primitive_type)
+    }
+    pub fn make_string_type() -> PropertyType {
+        make_primitive_type(make_string())
+    }
+    #[allow(unused)]
+    pub fn make_bool_type() -> PropertyType {
+        make_primitive_type(make_bool())
+    }
+    pub fn make_usize_type() -> PropertyType {
+        make_primitive_type(make_usize())
+    }
+    #[allow(unused)]
+    pub fn make_isize_type() -> PropertyType {
+        make_primitive_type(make_isize())
+    }
+    #[allow(unused)]
+    pub fn make_float_type() -> PropertyType {
+        make_primitive_type(make_float())
+    }
+    pub fn make_any() -> PropertyType {
+        PropertyType::Any
+    }
+    fn make_string() -> PrimitiveType {
+        PrimitiveType::String
+    }
+    fn make_bool() -> PrimitiveType {
+        PrimitiveType::Boolean
+    }
+    fn make_usize() -> PrimitiveType {
+        PrimitiveType::Number(Number::Usize)
+    }
+    fn make_isize() -> PrimitiveType {
+        PrimitiveType::Number(Number::Isize)
+    }
+    fn make_float() -> PrimitiveType {
+        PrimitiveType::Number(Number::Float)
     }
 }
