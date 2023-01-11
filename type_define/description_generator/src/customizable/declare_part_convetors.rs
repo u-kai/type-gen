ff4d30a2b5e1e75397746f85cf5ab06c57811292
u-kai <76635578+u-kai@users.noreply.github.com>
2023-01-11 22:16:22 +0900

    add alias_type_converotor.black_list and white_list

--- type_define/description_generator/src/customizable/declare_part_convetors.rs
+++ type_define/description_generator/src/customizable/declare_part_convetors.rs
@@ -1,5 +1,3 @@
-use super::declare_part_generator::CompositeTypeDeclareConvertor;
-
 pub struct BlackListConvertor<'a> {
     store: Vec<&'a str>,
 }
@@ -16,17 +14,6 @@ impl<'a> BlackListConvertor<'a> {
     }
 }
 
-impl<'a> CompositeTypeDeclareConvertor for BlackListConvertor<'a> {
-    fn convert(
-        &self,
-        acc: &mut String,
-        composite_type: &structure::composite_type_structure::CompositeTypeStructure,
-    ) -> () {
-        if self.containe_list(composite_type.type_name().as_str()) {
-            *acc = String::new()
-        }
-    }
-}
 pub struct WhiteListConvertor<'a> {
     store: Vec<&'a str>,
 }
@@ -43,19 +30,61 @@ impl<'a> WhiteListConvertor<'a> {
     }
 }
 
-impl<'a> CompositeTypeDeclareConvertor for WhiteListConvertor<'a> {
-    fn convert(
-        &self,
-        acc: &mut String,
-        composite_type: &structure::composite_type_structure::CompositeTypeStructure,
-    ) -> () {
-        if !self.containe_list(composite_type.type_name().as_str()) {
-            *acc = String::new()
+pub mod composite_type {
+    use crate::customizable::declare_part_generator::CompositeTypeDeclareConvertor;
+
+    use super::*;
+    impl<'a> CompositeTypeDeclareConvertor for BlackListConvertor<'a> {
+        fn convert(
+            &self,
+            acc: &mut String,
+            composite_type: &structure::composite_type_structure::CompositeTypeStructure,
+        ) -> () {
+            if self.containe_list(composite_type.type_name().as_str()) {
+                *acc = String::new()
+            }
+        }
+    }
+
+    impl<'a> CompositeTypeDeclareConvertor for WhiteListConvertor<'a> {
+        fn convert(
+            &self,
+            acc: &mut String,
+            composite_type: &structure::composite_type_structure::CompositeTypeStructure,
+        ) -> () {
+            if !self.containe_list(composite_type.type_name().as_str()) {
+                *acc = String::new()
+            }
+        }
+    }
+}
+pub mod alias_type {
+    use structure::alias_type_structure::AliasTypeStructure;
+
+    use crate::customizable::declare_part_generator::AliasTypeDeclareConvertor;
+
+    use super::{BlackListConvertor, WhiteListConvertor};
+
+    impl<'a> AliasTypeDeclareConvertor for BlackListConvertor<'a> {
+        fn convert(&self, acc: &mut String, alias_type: &AliasTypeStructure) -> () {
+            if self.containe_list(alias_type.type_name().as_str()) {
+                *acc = String::new()
+            }
+        }
+    }
+
+    impl<'a> AliasTypeDeclareConvertor for WhiteListConvertor<'a> {
+        fn convert(&self, acc: &mut String, alias_type: &AliasTypeStructure) -> () {
+            if !self.containe_list(alias_type.type_name().as_str()) {
+                *acc = String::new()
+            }
         }
     }
 }
 #[cfg(test)]
 mod composite_case_test {
+    use crate::customizable::declare_part_generator::CompositeTypeDeclareConvertor;
+
     use super::*;
     use std::collections::BTreeMap;
     use structure::composite_type_structure::CompositeTypeStructure;
@@ -99,6 +128,59 @@ mod composite_case_test {
         let white_list = WhiteListConvertor::new();
         let dummy_composite_type = CompositeTypeStructure::new(name, BTreeMap::new());
         white_list.convert(&mut acc, &dummy_composite_type);
+        assert_eq!(acc, tobe);
+    }
+}
+
+#[cfg(test)]
+mod alias_case_test {
+    use crate::customizable::declare_part_generator::AliasTypeDeclareConvertor;
+
+    use super::*;
+    use structure::{
+        alias_type_structure::AliasTypeStructure,
+        parts::property_type::property_type_factories::make_string_type,
+    };
+    #[test]
+    fn test_black_list_convertor_case_containe() {
+        let name = "Test";
+        let mut acc = String::from("type Test = String;");
+        let tobe = String::new();
+        let mut black_list = BlackListConvertor::new();
+        black_list.add(name);
+        let dummy_alias_type = AliasTypeStructure::new(name, make_string_type());
+        black_list.convert(&mut acc, &dummy_alias_type);
+        assert_eq!(acc, tobe);
+    }
+    #[test]
+    fn test_black_list_convertor_case_not_containe() {
+        let name = "Test";
+        let mut acc = String::from("type Test = String;");
+        let tobe = acc.clone();
+        let black_list = BlackListConvertor::new();
+        let dummy_alias_type = AliasTypeStructure::new(name, make_string_type());
+        black_list.convert(&mut acc, &dummy_alias_type);
+        assert_eq!(acc, tobe);
+    }
+    #[test]
+    fn test_white_list_convertor_case_containe() {
+        let name = "Test";
+        let mut acc = String::from("type Test = String;");
+        let tobe = acc.clone();
+        let mut white_list = WhiteListConvertor::new();
+        white_list.add(name);
+        let dummy_alias_type = AliasTypeStructure::new(name, make_string_type());
+        white_list.convert(&mut acc, &dummy_alias_type);
+        assert_eq!(acc, tobe);
+    }
+    #[test]
+    fn test_white_list_convertor_case_not_containe() {
+        let name = "Test";
+        let mut acc = String::from("type Test = String;");
+        let tobe = String::new();
+        let white_list = WhiteListConvertor::new();
+        let dummy_alias_type = AliasTypeStructure::new(name, make_string_type());
+        white_list.convert(&mut acc, &dummy_alias_type);
         assert_eq!(acc, tobe);
     }
 }
