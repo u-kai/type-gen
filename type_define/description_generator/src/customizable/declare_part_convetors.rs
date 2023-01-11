755ec1577158ff5a468781a95266faa8cccbb871
u-kai <76635578+u-kai@users.noreply.github.com>
2023-01-11 22:07:27 +0900

    add composite_type_converotor.black_list

--- type_define/description_generator/src/customizable/declare_part_convetors.rs
+++ type_define/description_generator/src/customizable/declare_part_convetors.rs
@@ -1,5 +1,32 @@
 use super::declare_part_generator::CompositeTypeDeclareConvertor;
 
+pub struct BlackListConvertor<'a> {
+    store: Vec<&'a str>,
+}
+
+impl<'a> BlackListConvertor<'a> {
+    pub fn new() -> Self {
+        Self { store: Vec::new() }
+    }
+    pub fn add(&mut self, type_name: &'a str) {
+        self.store.push(type_name)
+    }
+    fn containe_list(&self, type_name: &'a str) -> bool {
+        self.store.contains(&type_name)
+    }
+}
+
+impl<'a> CompositeTypeDeclareConvertor for BlackListConvertor<'a> {
+    fn convert(
+        &self,
+        acc: &mut String,
+        composite_type: &structure::composite_type_structure::CompositeTypeStructure,
+    ) -> () {
+        if self.containe_list(composite_type.type_name().as_str()) {
+            *acc = String::new()
+        }
+    }
+}
 pub struct WhiteListConvertor<'a> {
     store: Vec<&'a str>,
 }
@@ -32,6 +59,27 @@ mod composite_case_test {
     use super::*;
     use std::collections::BTreeMap;
     use structure::composite_type_structure::CompositeTypeStructure;
+    #[test]
+    fn test_black_list_convertor_case_containe() {
+        let name = "Test";
+        let mut acc = String::from("type Test = String;");
+        let tobe = String::new();
+        let mut black_list = BlackListConvertor::new();
+        black_list.add(name);
+        let dummy_composite_type = CompositeTypeStructure::new(name, BTreeMap::new());
+        black_list.convert(&mut acc, &dummy_composite_type);
+        assert_eq!(acc, tobe);
+    }
+    #[test]
+    fn test_black_list_convertor_case_not_containe() {
+        let name = "Test";
+        let mut acc = String::from("type Test = String;");
+        let tobe = acc.clone();
+        let black_list = BlackListConvertor::new();
+        let dummy_composite_type = CompositeTypeStructure::new(name, BTreeMap::new());
+        black_list.convert(&mut acc, &dummy_composite_type);
+        assert_eq!(acc, tobe);
+    }
     #[test]
     fn test_white_list_convertor_case_containe() {
         let name = "Test";
