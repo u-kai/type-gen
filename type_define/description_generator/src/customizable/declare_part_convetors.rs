0490ba7468341e53ab769b42de3d1e3c2fe06a7f
u-kai <76635578+u-kai@users.noreply.github.com>
2023-01-11 22:33:44 +0900

    AliasType.AddHeaderConvertor

--- type_define/description_generator/src/customizable/declare_part_convetors.rs
+++ type_define/description_generator/src/customizable/declare_part_convetors.rs
@@ -1,3 +1,21 @@
+pub struct AddHeaderConvertor<'a> {
+    header: &'a str,
+    store: Vec<&'a str>,
+}
+impl<'a> AddHeaderConvertor<'a> {
+    pub fn new(header: &'a str) -> Self {
+        Self {
+            header,
+            store: Vec::new(),
+        }
+    }
+    pub fn add(&mut self, type_name: impl Into<&'a str>) {
+        self.store.push(type_name.into());
+    }
+    fn containe_list(&self, type_name: &'a str) -> bool {
+        self.store.contains(&type_name)
+    }
+}
 pub struct BlackListConvertor<'a> {
     store: Vec<&'a str>,
 }
@@ -31,9 +49,9 @@ impl<'a> WhiteListConvertor<'a> {
 }
 
 pub mod composite_type {
+    use super::*;
     use crate::customizable::declare_part_generator::CompositeTypeDeclareConvertor;
 
-    use super::*;
     impl<'a> CompositeTypeDeclareConvertor for BlackListConvertor<'a> {
         fn convert(
             &self,
@@ -59,12 +77,19 @@ pub mod composite_type {
     }
 }
 pub mod alias_type {
+    use super::{AddHeaderConvertor, BlackListConvertor, WhiteListConvertor};
+    use crate::customizable::declare_part_generator::{
+        AliasTypeDeclareConvertor, AliasTypeIdentifyConvertor,
+    };
     use structure::alias_type_structure::AliasTypeStructure;
 
-    use crate::customizable::declare_part_generator::AliasTypeDeclareConvertor;
-
-    use super::{BlackListConvertor, WhiteListConvertor};
-
+    impl<'a> AliasTypeIdentifyConvertor for AddHeaderConvertor<'a> {
+        fn convert(&self, acc: &mut String, alias_type: &AliasTypeStructure) -> () {
+            if self.containe_list(alias_type.type_name().as_str()) {
+                *acc = format!("{}{}", self.header, acc)
+            }
+        }
+    }
     impl<'a> AliasTypeDeclareConvertor for BlackListConvertor<'a> {
         fn convert(&self, acc: &mut String, alias_type: &AliasTypeStructure) -> () {
             if self.containe_list(alias_type.type_name().as_str()) {
@@ -134,13 +159,37 @@ mod composite_case_test {
 
 #[cfg(test)]
 mod alias_case_test {
-    use crate::customizable::declare_part_generator::AliasTypeDeclareConvertor;
+    use crate::customizable::declare_part_generator::{
+        AliasTypeDeclareConvertor, AliasTypeIdentifyConvertor,
+    };
 
     use super::*;
     use structure::{
         alias_type_structure::AliasTypeStructure,
         parts::property_type::property_type_factories::make_string_type,
     };
+    #[test]
+    fn test_add_header_case_containe() {
+        let mut acc = String::from("type Test = String;");
+        let tobe = format!("pub {}", acc);
+        let mut add_header_convertor = AddHeaderConvertor::new("pub ");
+        let name = "Test";
+        add_header_convertor.add(name);
+        let dummy_alias_type = AliasTypeStructure::new(name, make_string_type());
+        add_header_convertor.convert(&mut acc, &dummy_alias_type);
+        assert_eq!(acc, tobe)
+    }
+    #[test]
+    fn test_add_header_case_not_containe() {
+        let mut acc = String::from("type Test = String;");
+        let tobe = acc.clone();
+        let mut add_header_convertor = AddHeaderConvertor::new("pub ");
+        let name = "Test";
+        add_header_convertor.add(name);
+        let dummy_alias_type = AliasTypeStructure::new("NotContaine", make_string_type());
+        add_header_convertor.convert(&mut acc, &dummy_alias_type);
+        assert_eq!(acc, tobe)
+    }
     #[test]
     fn test_black_list_convertor_case_containe() {
         let name = "Test";
