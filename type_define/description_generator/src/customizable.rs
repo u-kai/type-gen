f10e99276380a373f5cec06c17a7242442e9b3a3
u-kai <76635578+u-kai@users.noreply.github.com>
2023-01-11 22:05:17 +0900

    add composite_type_converotor.white_list

--- type_define/description_generator/src/customizable.rs
+++ type_define/description_generator/src/customizable.rs
@@ -1,2 +1,3 @@
+pub mod declare_part_convetors;
 pub mod declare_part_generator;
 pub mod property_part_generator;
---
+++ type_define/description_generator/src/customizable/declare_part_convetors.rs
@@ -0,0 +1,56 @@
+use super::declare_part_generator::CompositeTypeDeclareConvertor;
+
+pub struct WhiteListConvertor<'a> {
+    store: Vec<&'a str>,
+}
+
+impl<'a> WhiteListConvertor<'a> {
+    pub fn new() -> Self {
+        Self { store: Vec::new() }
+    }
+    pub fn add(&mut self, type_name: &'a str) {
+        self.store.push(type_name)
+    }
+    fn containe_list(&self, type_name: &'a str) -> bool {
+        self.store.contains(&type_name)
+    }
+}
+
+impl<'a> CompositeTypeDeclareConvertor for WhiteListConvertor<'a> {
+    fn convert(
+        &self,
+        acc: &mut String,
+        composite_type: &structure::composite_type_structure::CompositeTypeStructure,
+    ) -> () {
+        if !self.containe_list(composite_type.type_name().as_str()) {
+            *acc = String::new()
+        }
+    }
+}
+#[cfg(test)]
+mod composite_case_test {
+    use super::*;
+    use std::collections::BTreeMap;
+    use structure::composite_type_structure::CompositeTypeStructure;
+    #[test]
+    fn test_white_list_convertor_case_containe() {
+        let name = "Test";
+        let mut acc = String::from("type Test = String;");
+        let tobe = acc.clone();
+        let mut white_list = WhiteListConvertor::new();
+        white_list.add(name);
+        let dummy_composite_type = CompositeTypeStructure::new(name, BTreeMap::new());
+        white_list.convert(&mut acc, &dummy_composite_type);
+        assert_eq!(acc, tobe);
+    }
+    #[test]
+    fn test_white_list_convertor_case_not_containe() {
+        let name = "Test";
+        let mut acc = String::from("type Test = String;");
+        let tobe = String::new();
+        let white_list = WhiteListConvertor::new();
+        let dummy_composite_type = CompositeTypeStructure::new(name, BTreeMap::new());
+        white_list.convert(&mut acc, &dummy_composite_type);
+        assert_eq!(acc, tobe);
+    }
+}
