2bca141c9e8286e259d444363c9e9cc013d78432
u-kai <76635578+u-kai@users.noreply.github.com>
2023-02-18 16:13:21 +0900

    add go mapper

---
+++ langs/go/src/description_generator/mapper.rs
@@ -0,0 +1,48 @@
+use description_generator::type_mapper::{TypeMapper, TypeString};
+use structure::parts::type_name::TypeName;
+
+pub struct GoMapper;
+
+impl TypeMapper for GoMapper {
+    fn case_string(&self) -> TypeString {
+        "string".to_string()
+    }
+    fn case_null(&self) -> TypeString {
+        self.case_any()
+    }
+    fn case_custom_type(&self, custom_type: &TypeName) -> String {
+        replace_cannot_use_char(custom_type.as_str())
+    }
+    fn case_any(&self) -> TypeString {
+        "interface {}".to_string()
+    }
+    fn case_boolean(&self) -> TypeString {
+        "bool".to_string()
+    }
+    fn case_array_type<T: Into<TypeString>>(&self, type_statement: T) -> TypeString {
+        format!("[]{}", type_statement.into())
+    }
+    fn case_optional_type<T: Into<TypeString>>(&self, _type_statement: T) -> TypeString {
+        todo!("go is not defined optional")
+        //format!("", type_statement.into())
+    }
+    fn case_float(&self) -> TypeString {
+        "float64".to_string()
+    }
+    fn case_isize(&self) -> TypeString {
+        "int".to_string()
+    }
+    fn case_usize(&self) -> TypeString {
+        "int".to_string()
+    }
+}
+fn replace_cannot_use_char(str: &str) -> String {
+    str.replace(cannot_use_char, "")
+}
+fn cannot_use_char(c: char) -> bool {
+    match c {
+        ':' | ';' | '#' | '$' | '%' | '&' | '~' | '=' | '|' | '\"' | '\'' | '{' | '}' | '?'
+        | '!' | '<' | '>' | '[' | ']' | '*' | '^' => true,
+        _ => false,
+    }
+}
