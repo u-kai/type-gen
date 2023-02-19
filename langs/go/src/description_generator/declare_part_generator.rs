89373fecf08b233aa35513e21c88d71bdcb6423b
u-kai <76635578+u-kai@users.noreply.github.com>
2023-02-19 10:28:46 +0900

    add go alias type

--- langs/go/src/description_generator/declare_part_generator.rs
+++ langs/go/src/description_generator/declare_part_generator.rs
@@ -48,7 +48,7 @@ impl DeclarePartGenerator for GoDeclarePartGenerator {
         alias_type: &structure::alias_type_structure::AliasTypeStructure,
         mapper: &Self::Mapper,
     ) -> String {
-        todo!();
+        self.inner.generate_case_alias(alias_type, mapper)
     }
 }
 
@@ -56,9 +56,22 @@ impl DeclarePartGenerator for GoDeclarePartGenerator {
 mod tests {
     use std::collections::BTreeMap;
 
-    use structure::composite_type_structure::CompositeTypeStructure;
+    use structure::{
+        alias_type_structure::AliasTypeStructure,
+        composite_type_structure::CompositeTypeStructure,
+        parts::property_type::property_type_factories::{make_primitive_type, make_string_type},
+    };
 
     use super::*;
+    #[test]
+    fn カスタム型のalias型定義の作成() {
+        let sut = GoDeclarePartGenerator::new();
+        let mapper = GoMapper {};
+        let alias = AliasTypeStructure::new("Test", make_string_type());
+        let result = sut.generate_case_alias(&alias, &mapper);
+        println!("{}", result);
+        assert_eq!(result, "type Test string");
+    }
     #[test]
     fn idという整数型のプロパティを持つ型定義の作成() {
         let type_name = TypeName::new("Test");
