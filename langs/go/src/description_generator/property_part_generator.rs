f42963ee1c9e499d50a8df5fd3121ae25dd19b5a
u-kai <76635578+u-kai@users.noreply.github.com>
2023-03-11 09:32:11 +0900

    add go optional

--- langs/go/src/description_generator/property_part_generator.rs
+++ langs/go/src/description_generator/property_part_generator.rs
@@ -51,14 +51,43 @@ impl GoPropertyPartGeneratorBuilder {
                 _type_name: &structure::parts::type_name::TypeName,
                 property_key: &structure::parts::property_key::PropertyKey,
                 _property_type: &structure::parts::property_type::PropertyType,
-                __mapper: &GoMapper,
+                _mapper: &GoMapper,
             ) -> () {
-                *acc = format!(r#"{} `json:"{}"`"#, acc, property_key.as_str())
+                if acc.contains("`json:") {
+                    return;
+                } else {
+                    *acc = format!(r#"{} `json:"{}"`"#, acc, property_key.as_str())
+                }
             }
         }
         self.generator
             .inner
             .add_property_type_convertor(Box::new(AddJsonMarshalConvertor {}));
+        self
+    }
+    pub fn all_optional(mut self) -> Self {
+        struct AddJsonMarshalOptionalConvertor {}
+        impl Convertor<GoMapper> for AddJsonMarshalOptionalConvertor {
+            fn convert(
+                &self,
+                acc: &mut String,
+                _type_name: &structure::parts::type_name::TypeName,
+                property_key: &structure::parts::property_key::PropertyKey,
+                _property_type: &structure::parts::property_type::PropertyType,
+                _mapper: &GoMapper,
+            ) -> () {
+                if acc.contains("`json:") {
+                    let from = format!(r#"`json:"{}"`"#, property_key.as_str());
+                    let to = format!(r#"`json:"{},omitempty"`"#, property_key.as_str());
+                    *acc = acc.replace(&from, &to)
+                } else {
+                    *acc = format!(r#"{} `json:"{},omitempty"`"#, acc, property_key.as_str())
+                }
+            }
+        }
+        self.generator
+            .inner
+            .add_property_type_convertor(Box::new(AddJsonMarshalOptionalConvertor {}));
         self
     }
     pub fn pub_all(mut self) -> Self {
@@ -98,6 +127,41 @@ mod tests {
         property_part_generator::{GoPropertyPartGenerator, GoPropertyPartGeneratorBuilder},
     };
 
+    #[test]
+    fn optionalの設定だけでjsonの設定も行われる() {
+        let type_name: TypeName = "Test".into();
+        let property_key: PropertyKey = "id".into();
+        let property_type = make_usize_type();
+        let mapper = GoMapper;
+
+        let sut = GoPropertyPartGeneratorBuilder::new()
+            .pub_all()
+            .all_optional()
+            .build();
+        assert_eq!(
+            sut.generate(&type_name, &property_key, &property_type, &mapper,),
+            r#"   Id int `json:"id,omitempty"`
+"#
+        );
+    }
+    #[test]
+    fn 全てのプロパティをoptionalかつパブリックにしてjsonの元の名前を記述する() {
+        let type_name: TypeName = "Test".into();
+        let property_key: PropertyKey = "id".into();
+        let property_type = make_usize_type();
+        let mapper = GoMapper;
+
+        let sut = GoPropertyPartGeneratorBuilder::new()
+            .pub_all()
+            .json_marshal()
+            .all_optional()
+            .build();
+        assert_eq!(
+            sut.generate(&type_name, &property_key, &property_type, &mapper,),
+            r#"   Id int `json:"id,omitempty"`
+"#
+        );
+    }
     #[test]
     fn 全てのプロパティをパブリックにしてjsonの元の名前を記述する() {
         let type_name: TypeName = "Test".into();
